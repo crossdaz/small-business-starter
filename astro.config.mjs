c392b6dc9baf12ce5e400e021e671d32e130d1c8
crossdaz <mail@darrencross.co.uk>
2026-06-09 09:08:47 +0100

    debug sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,3 +39,13 @@ export default defineConfig({
     },
   },
 });
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  }
+});
